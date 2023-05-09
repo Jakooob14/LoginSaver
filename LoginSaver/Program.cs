24bc2a23151d8ac79c5263af36e99dc011b71f4f
Jakooob <sokoljakub14@gmail.com>
2023-05-09 12:30:54 +0200

    Initial commit

---
+++ LoginSaver/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Spectre.Console;
+
+namespace LoginSaver
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WindowWidth = 100;
+            Console.WindowHeight = 25;
+            AnsiConsole.Write(new Markup("[bold yellow]Login Saver v420.69[/]"));
+            
+            var fruit = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("Choose a platform")
+                    .PageSize(10)
+                    .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
+                    .AddChoices(new[] {
+                        "Apple", "Apricot", "Avocado", 
+                        "Banana", "Blackcurrant", "Blueberry",
+                        "Cherry", "Cloudberry", "Cocunut",
+                    }));
+
+// Echo the fruit back to the terminal
+            AnsiConsole.WriteLine($"I agree. {fruit} is tasty!");
+        }
+    }
+}
---
+++ LoginSaver/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LoginSaver")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LoginSaver")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0B940617-86F4-405E-8CDF-2B67D1D1CA14")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
